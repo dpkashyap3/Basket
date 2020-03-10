4685a65ad1e0d91eaad26da0cf630c8de6ae0503
dpkashyap3 <32429850+dpkashyap3@users.noreply.github.com>
2020-03-10 15:01:31 +0530

    Updates

---
+++ precache-manifest.52886f98eb0d804764ae230905e28228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b704576bb4d365e42045b88d667ba0",
+    "url": "/basket/index.html"
+  },
+  {
+    "revision": "5ea3ab7805038d49a33a",
+    "url": "/basket/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "57fb06d62ec1032be087",
+    "url": "/basket/static/css/main.e8189bf0.chunk.css"
+  },
+  {
+    "revision": "5ea3ab7805038d49a33a",
+    "url": "/basket/static/js/2.cf9c4671.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/basket/static/js/2.cf9c4671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fb06d62ec1032be087",
+    "url": "/basket/static/js/main.97be5c77.chunk.js"
+  },
+  {
+    "revision": "02a36044d8eb4c19b9a4",
+    "url": "/basket/static/js/runtime-main.40c0eccd.js"
+  }
+]);
